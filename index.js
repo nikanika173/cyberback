e6c498415b869e17b1377b5f131e1e8e88ec8fad
nikanika173 <147316302+nikanika173@users.noreply.github.com>
2025-03-14 01:44:35 +0400

    Add files via upload

--- index.js
+++ index.js
@@ -4,25 +4,59 @@ import {connectToDatabase} from "./services/database.js";
 import bodyParser from "body-parser";
 
 const app = express();
-const PORT = process.env.Port || 5000
+const PORT = process.env.PORT || 5000;
 
 app.use(express.json());
 app.use(cors());
-app.use(bodyParser.urlencoded({ extended: false }))
+app.use(bodyParser.urlencoded({extended: false}))
 
 app.get('/', async (req, res) => {
+  async function run() {
     try {
-        const database = await connectToDatabase()
+      const database = await connectToDatabase();
+      res.json({status: "Server is running!"});
     } catch (error) {
-        console.log('error handling request:', error);
-        res.status(500).json({ error: 'error'})
+      console.error('Error handling request:', error);
+      res.status(500).json({error: 'Internal server error'});
     }
-    run().cath(console.dir)
-    res.json({status: "server is running!"})
+  }
+  run().catch(console.dir);
+});
 
-})
+app.get('/menus/navbarItems', async (req, res) => {
+  async function run() {
+    try {
+      const database = await connectToDatabase();
+      const navbarItems = database.collection('navbar_items');
+      const cursor = navbarItems.find();
+      const result = await cursor.toArray()
+      res.json(result);
+    } catch (error) {
+      console.error('Error handling request:', error);
+      res.status(500).json({error: 'Internal server error'});
+    }
+  }
+  run().catch(console.dir);
+});
 
+// app.get('/', async (req, res) => {
+//   async function run() {
+//     try {
+//       const database = await connectToDatabase();
+//       const users = database.collection('users');
+//       const cursor = users.find();
+//       const result = await cursor.toArray()
+//
+//       res.json(result);
+//     } catch (error) {
+//       console.error('Error handling request:', error);
+//       res.status(500).json({error: 'Internal server error'});
+//     }
+//   }
+//
+//   run().catch(console.dir);
+// });
 
 app.listen(PORT, () => {
-    console.log(`Server is running on port ${PORT}`);
-  });
+  console.log(`Server is running on port ${PORT}`);
+});
