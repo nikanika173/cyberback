f199742e5b1567dad2d131a0b5083a773935375c
nikanika173 <147316302+nikanika173@users.noreply.github.com>
2025-03-17 21:46:39 +0400

    Add files via upload

--- index.js
+++ index.js
@@ -39,6 +39,29 @@ app.get('/menus/navbarItems', async (req, res) => {
   run().catch(console.dir);
 });
 
+app.get("/categories", async (req, res) => {
+  async function run() {
+    try {
+      const database = await connectToDatabase();
+      const categoryDb = database.db("category_db");
+
+      const categories = ["Gaming", "cameras", "computers", "headphones", "phones", "smartwatches"];
+      let data = {};
+
+      for (const category of categories) {
+        const collection = categoryDb.collection(category);
+        const result = await collection.find().toArray();
+        data[category] = result;
+      }
+
+      res.json(data);
+    } catch (error) {
+      console.error("Error handling request:", error);
+      res.status(500).json({ error: "Internal server error" });
+    }
+  }
+  run().catch(console.dir);
+});
 
 app.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}`);
